7b725997f88cab781e2c684cd5d1873e4f5f5210
Joceph Erickson Dela Cruz <42251119+jocephdelacruz@users.noreply.github.com>
2024-03-04 10:32:34 +0800

    added subnet and keypair info

--- main.tf
+++ main.tf
@@ -18,13 +18,29 @@ data "aws_vpc" "default" {
   id = "vpc-00613fdde619e4f7b"
 }
 
+data "aws_subnet" "snt_2a_pub" {
+  id = "subnet-0d477a46d276ecfca"
+}
+
+data "aws_key_pair" "tf_kp_sgdevops" {
+  key_name           = "sgdevops"
+  include_public_key = true
+}
+
 resource "aws_instance" "web" {
-  ami  = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.tf_sg_default.id]
+  subnet_id              = aws_subnet.snt_2a_pub.id
+  key_name               = data.aws_key_pair.tf_kp_sgdevops.key_name
+  
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
 
   tags = {
-    Name = "HelloWorld"
+    Name = "intelyse-dev-ecs"
   }
 }
 
