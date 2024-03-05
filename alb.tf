265edc1cc0581de642a00c431e561f7d171d46df
Joceph Erickson Dela Cruz <42251119+jocephdelacruz@users.noreply.github.com>
2024-03-06 00:58:24 +0800

    fix issue with asg 9

--- alb.tf
+++ alb.tf
@@ -1,6 +1,7 @@
 module "alb_tf_module" {
   source = "terraform-aws-modules/alb/aws"
-
+	version = "9.7.0"
+	
   name            = "alb-tf-module"
   vpc_id          = module.vpc_tf_module.vpc_id
   subnets         = module.vpc_tf_module.public_subnets
