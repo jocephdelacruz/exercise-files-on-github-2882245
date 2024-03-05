067bf23c4ceaace002e01f083dcea1d79dd275b5
Joceph Erickson Dela Cruz <42251119+jocephdelacruz@users.noreply.github.com>
2024-03-05 16:14:52 +0800

    utilize new vpc using module

--- main.tf
+++ main.tf
@@ -14,8 +14,26 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"]  # Bitnami
 }
 
-data "aws_vpc" "default" {
-  id = "vpc-00613fdde619e4f7b"
+#data "aws_vpc" "default" {
+#  id = "vpc-00613fdde619e4f7b"
+#}
+
+module "vpc_tf_module" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.2"
+
+  name = "vpc_tf_module"
+  cidr = "10.10.0.0/16"
+
+  azs = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  public_subnets = ["10.10.10.0/24", "10.10.11.0/24", "10.10.12.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 data "aws_subnet" "snt_2a_pub" {
@@ -49,7 +67,7 @@ module "sg_module" {
   version = "5.1.1"
   name = "tf_sg_module"
   
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc_tf_module.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -58,44 +76,44 @@ module "sg_module" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group" "tf_sg_default" {
-  name = "dev_default"
-  vpc_id = data.aws_vpc.default.id
+#resource "aws_security_group" "tf_sg_default" {
+#  name = "dev_default"
+#  vpc_id = data.aws_vpc.default.id
+#
+#  tags = {
+#    Name = "intelyse-dev-ecs"
+#  }
+#}
 
-  tags = {
-    Name = "intelyse-dev-ecs"
-  }
-}
+#resource "aws_vpc_security_group_ingress_rule" "tf_sg_ing_http" {
+#  security_group_id = aws_security_group.tf_sg_default.id
+#
+#  cidr_ipv4   = "0.0.0.0/0"
+#  from_port   = 80
+#  ip_protocol = "tcp"
+#  to_port     = 80
+#}
 
-resource "aws_vpc_security_group_ingress_rule" "tf_sg_ing_http" {
-  security_group_id = aws_security_group.tf_sg_default.id
+#resource "aws_vpc_security_group_ingress_rule" "tf_sg_ing_https" {
+#  security_group_id = aws_security_group.tf_sg_default.id
+#
+#  cidr_ipv4   = "0.0.0.0/0"
+#  from_port   = 443
+#  ip_protocol = "tcp"
+#  to_port     = 443
+#}
 
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
-}
+#resource "aws_vpc_security_group_ingress_rule" "tf_sg_ing_ssh" {
+#  security_group_id = aws_security_group.tf_sg_default.id
+#
+#  cidr_ipv4   = "0.0.0.0/0"
+#  from_port   = 22
+#  ip_protocol = "tcp"
+#  to_port     = 22
+#}
 
-resource "aws_vpc_security_group_ingress_rule" "tf_sg_ing_https" {
-  security_group_id = aws_security_group.tf_sg_default.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 443
-  ip_protocol = "tcp"
-  to_port     = 443
-}
-
-resource "aws_vpc_security_group_ingress_rule" "tf_sg_ing_ssh" {
-  security_group_id = aws_security_group.tf_sg_default.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 22
-  ip_protocol = "tcp"
-  to_port     = 22
-}
-
-resource "aws_vpc_security_group_egress_rule" "tf_sg_egr_all" {
-  security_group_id = aws_security_group.tf_sg_default.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+#resource "aws_vpc_security_group_egress_rule" "tf_sg_egr_all" {
+#  security_group_id = aws_security_group.tf_sg_default.id
+#  cidr_ipv4         = "0.0.0.0/0"
+#  ip_protocol       = "-1" # semantically equivalent to all ports
+#}
